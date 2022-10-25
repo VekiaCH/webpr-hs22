161db46a58e99361cd25a4eeab26ab757d8e4669
Kyle.Egli <Kyle.Egli@students.fhnw.ch>
2022-10-25 15:10:47 +0200

    week 3-4 done

--- week1/FunctionsTest/functionsTest.js
+++ week1/FunctionsTest/functionsTest.js
@@ -1,9 +1,9 @@
 const funArray = [];
 
-const T = x => y => x;
-const F = x => y => y;
+//const T = x => y => x;
+//const F = x => y => y;
 // WEEK 3
-funArray.push("WEEK 3");
+//funArray.push("WEEK 3");
 
 /* const pair = a => b => f => f(a)(b);
 const fst = p => p(T);
@@ -19,7 +19,7 @@ const sum = pairPlus(oneTwo)(oneTwo);
 const square = x => x * x;
 const result = pairMap(square) (oneTwo); */
 
-//fst(oneTwo) = 0;
+/*fst(oneTwo) = 0;
 const s1 = x => y => z => x;
 const s2 = x => y => z => y;
 const s3 = x => y => z => z;
@@ -30,7 +30,44 @@ const b = t => t(s2);
 const c = t => t(s3);
 
 const oneTwoThree = triple(1);
-funArray.push((a(oneTwoThree) === 1).toString());
+funArray.push((a(oneTwoThree) === 1).toString());*/
+
+/*const Left   = x => f => g => f (x);
+const Right  = x => f => g => g (x);
+const either = e => f => g => e (f) (g);
+const id = x => x;
+
+const x_ = Math.random();
+
+const safeDiv = num => divisor => 
+    divisor === 0 
+        ? Left("Cannot divide by 0")
+        : Right (num / divisor);
+
+const eShow = x => x((x) => x)((x) => "Result is: "+ x);
+const result = safeDiv(1)(1);
+
+either(result) 
+    (msg => funArray.push(msg))
+    (val => funArray.push(val===1));
+
+funArray.push(eShow(safeDiv(1)(0)) === "Cannot divide by 0" && eShow(safeDiv(x_)(1)) === "Result is: "+x_);
+*/
+
+//Week 4
+funArray.push("Week 4");
+
+const twice = x => x * 2;
+
+funArray.push([1,2,3].map(twice).toString() === "2,4,6");
+
+const divides = x => y => y%3 === 0;
+
+funArray.push([1,2,3,4,5,6].filter(divides(3)).toString() === "3,6");
+
+const join = z => (x,y) => x+z+y;
+
+funArray.push([1,2,3].reduce(join('-')));
 
 funArray.forEach((e, i) => {
     document.writeln(i + ": ");
